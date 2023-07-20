315788474cda44e88385f3d608ed5005bb3c3025
patriciadania <patricia.adania@hotmail.com>
2023-07-20 13:13:19 -0300

    Ajustes da estilização dos componentes

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../Imagem/Logo.png';
 import './Header.css';
 import CarFilter from '../Filtro/FiltroCar';
+import { FormControl, Select, MenuItem, styled } from '@mui/material';
 
-function Header({ onFilterChange, onCarsPerPageChange, carsPerPage }) {
+const StyledFormControl = styled(FormControl)(({ theme }) => ({
+  margin: theme.spacing(1),
+  minWidth: 20,
+  backgroundColor: 'rgb(51, 49, 49)',
+}));
+
+const StyledSelect = styled(Select)(() => ({
+  borderRadius: '10px',
+  border: 'solid ',
+  backgroundColor: 'rgb(51, 49, 49)',
+  color: '#fff',
+}));
+
+const StyledMenuItem = styled(MenuItem)(() => ({
+  padding: '5px 10px',
+}));
+
+function Header({ onFilterChange, onCarsPerPageChange, carsPerPage, onLayoutChange }) {
   const LogoClick = () => {
     window.location.reload();
+  };
+
+  const [selectedLayout, setSelectedLayout] = useState('vertical');
+
+  const LayoutChange = (event) => {
+    setSelectedLayout(event.target.value);
+    onLayoutChange(event.target.value);
   };
 
   return (
     <header className="header">
       <img className="logo" src={Logo} alt="Logo" onClick={LogoClick} />
-      <CarFilter
-        filtro={{
-          nome: '',
-          marca: 'todos',
-        }}
-        onFilterChange={onFilterChange}
-        onCarsPerPageChange={onCarsPerPageChange}
-        carsPerPage={carsPerPage}
-      />
+      <div className="filters-container">
+        <CarFilter
+          filtro={{
+            nome: '',
+            marca: 'todos',
+          }}
+          onFilterChange={onFilterChange}
+          onCarsPerPageChange={onCarsPerPageChange}
+          carsPerPage={carsPerPage}
+        />
+        <StyledFormControl>
+          <StyledSelect
+            value={selectedLayout}
+            onChange={LayoutChange}
+            inputProps={{
+              'aria-label': 'Layout',
+            }}
+          >
+            <StyledMenuItem value="vertical">Escolha outro layout</StyledMenuItem>
+            <StyledMenuItem value="horizontal">Horizontal</StyledMenuItem>
+          </StyledSelect>
+        </StyledFormControl>
+      </div>
     </header>
   );
 }
